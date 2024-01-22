dace7386da71877f727eaea578fb777cde47c42c
Armando Ruiz <ruiz@Armandos-MacBook-Pro.local>
2024-01-22 11:07:30 -0600

    add header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CommonModule } from '@angular/common';
 import type { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -16,6 +17,34 @@ export class ListComponent {
 
   constructor() {
     const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 20,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 45,
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 20,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 45,
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString()
+      },
       {
         id: Date.now(),
         title: 'Product 1',
