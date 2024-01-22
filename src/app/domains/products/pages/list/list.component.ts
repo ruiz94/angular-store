10fe0e0a7b27d86ed47f747039f6d6e28e94876f
Armando Ruiz <ruiz@Armandos-MacBook-Pro.local>
2024-01-22 10:31:08 -0600

    show products dynamically, type Products

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { CommonModule } from '@angular/common';
+import type { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 20,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 45,
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString()
+      },
+    ];
+    this.products.set(initProducts)
+  }
+
   fromChild(event: string){
     console.log("desde el padre: " + event);
 
